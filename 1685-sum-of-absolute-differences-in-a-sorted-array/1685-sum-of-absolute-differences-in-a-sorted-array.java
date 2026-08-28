d69f9678b74821de9f327c14e4f1b23058bc6298
Durgeshkumar-123 <vtu29843@veltech.edu.in>
2026-08-28 14:21:36 +0530

    Time: 4 ms (82.97%), Space: 85.2 MB (91.56%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.java
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.java
@@ -1,26 +1,21 @@
-import java.util.Arrays;
-
 class Solution {
     public int[] getSumAbsoluteDifferences(int[] nums) {
         int n = nums.length;
-        int[] ans = new int[n];
-        int totalSum = 0;
-        for (int num : nums) {
-            totalSum += num;
-        }
-        int leftSum = 0;
-        for (int i = 0; i < n; i++) {
-            int rightSum = totalSum - leftSum - nums[i];
-            ans[i] = nums[i] * i - leftSum
-                    + rightSum - nums[i] * (n - i - 1);
-            leftSum += nums[i];
-        }
-        return ans;
-    }
+        int[] result = new int[n];
+        long total = 0;
+        long prefix = 0;
 
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        int[] nums = { 2, 3, 5 };
-        System.out.println(Arrays.toString(s.getSumAbsoluteDifferences(nums)));
+        for (int x : nums)
+            total += x;
+
+        for (int i = 0; i < n; i++) {
+            long left = (long) nums[i] * i - prefix;
+            long right = (total - prefix - nums[i])
+                       - (long) nums[i] * (n - i - 1);
+
+            result[i] = (int)(left + right);
+            prefix += nums[i];
+        }
+        return result;
     }
 }
